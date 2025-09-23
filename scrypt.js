a3eec1677c78b8e5072e72f16895ad6ce9c9f5a0
JUND Guy <gjund@free.fr>
2025-09-23 11:45:30 +0200

    Login Form 2.1

--- scrypt.js
+++ scrypt.js
@@ -1,5 +1,26 @@
-let email = document.querySelector("#email")
-let password = document.querySelector("#password")
-let button = document.querySelector("#button")
-//console.log(email, password, button)
+let mail = document.querySelector("#email")
+let pwd = document.querySelector("#password")
+let btn = document.querySelector("#button")
+//console.log(mail, pwd, btn)
 
+btn.addEventListener("click", () => {
+    console.log("click")
+})
+
+mail.addEventListener("input", (event) => {
+    if (event.target.value.length === 0)
+        mail.style.borderColor = "black"
+    else if (!event.target.value.includes("@"))
+        mail.style.borderColor = "red"
+    else
+        mail.style.borderColor = "green"
+})
+
+pwd.addEventListener("input", (event) => {
+    if (event.target.value.length === 0)
+        pwd.style.borderColor = "black"
+    else if (event.target.value.length < 8)
+        pwd.style.borderColor = "red"
+    else
+        pwd.style.borderColor = "green"
+})
